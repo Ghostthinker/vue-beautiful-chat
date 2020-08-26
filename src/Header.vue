d42f1479c19ebaf1d58dd8058bf8222643b3619d
Lina Kuche <lina.kuche.gt@gmail.com>
2020-08-26 11:01:32 +0200

    header close not hover

--- src/Header.vue
+++ src/Header.vue
@@ -142,10 +142,6 @@ export default {
   margin-left: auto;
 }
 
-.sc-header--close-button:hover {
-  box-shadow: 0px 2px 5px rgba(0.2, 0.2, 0.5, 0.1);
-}
-
 .sc-header--close-button img {
   width: 100%;
   height: 100%;
